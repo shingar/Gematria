51cecfcf0d64a349589503200ebdd0cabff29e3e
visualstudio <visualstudio@122-PC30>
2019-04-12 18:46:06 -0400

    section1

--- Gematria/Program.cs
+++ Gematria/Program.cs
@@ -8,32 +8,35 @@ namespace Gematria
 {
     class Program
     {
-        String[] Letters = new string[26] { "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
-        static void Main()
-        {
-            Program orange = new Program();
-            Console.WriteLine(orange.Gematria("Universe"));
+        string[] Letters = new string[26] {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "1",
+"m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z" };
 
+        public static void Main()
+        {
+
+            Program orange = new Program();
+            Console.WriteLine(orange.Gematria("universe"));
+            Console.ReadLine();
         }
+
         public int Gematria(string word)
         {
+
             int GematriaValue = 0;
             for (int i = 0; i < word.Length; i++)
-            {
-                GematriaValue += LetterValue(word[i].ToString());
-            }
+            { GematriaValue += LetterValue(word[i].ToString()); }
             return GematriaValue;
         }
+
         public int LetterValue(string letter)
         {
             int x = 0;
             while (Letters[x] != letter)
             {
-                if (Letters[x++] == letter)
-                    return x;
+                if (Letters[x++] == letter) return -1;
             }
-            return -1;
+            return x + 1;
         }
-
     }
 }
+
